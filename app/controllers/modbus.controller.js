bb974375dbff6b146227fbe00df43f133f93c78e
Thanapon Yalan <thanaponyalan@Thanapons-MacBook-Pro.local>
2020-11-18 16:20:54 +0700

    read state

--- app/controllers/modbus.controller.js
+++ app/controllers/modbus.controller.js
@@ -1,8 +1,13 @@
 var modbusRTU=require('../../config/modbus');
 
 var toggleRelay=(req,res)=>{
-    modbusRTU.writeCoil(req.params['ch'],1);
-    res.send(1);
+    modbusRTU.readCoils(req.params['ch'],1).then((data)=>{
+        modbusRTU.writeCoil(req.params['ch'],!data);
+        res.send("COMPLETE");
+    }).catch((err)=>{
+        console.log(err);
+        res.send("ERROR")
+    });
 }
 
 module.exports={
