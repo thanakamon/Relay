9e6b5015fe6c8a63060fe2aa8febb3cd21d02845
Thanapon Yalan <thanaponyalan@Thanapons-MacBook-Pro.local>
2020-11-18 16:08:14 +0700

    config

--- config/modbus.js
+++ config/modbus.js
@@ -1,5 +1,5 @@
 var ModbusRTU=require('modbus-serial');
-var client=ModbusRTU();
+var client=new ModbusRTU();
 //const serialPort="/dev/tty.usbserial-1410";
 const serialPort="/dev/ttyUSB0";
 client.connectRTU(serialPort,{buadRate:9600});
