12faa38c2378c9d3bc9151c31432ad15e903d8be
thanaponyalan <42196077+thanaponyalan@users.noreply.github.com>
2020-11-24 01:39:46 +0700

    Update modbusRTU.js

--- public/modbusRTU.js
+++ public/modbusRTU.js
@@ -8,4 +8,23 @@ angular.module('app',[])
             console.log(err);
         })
     }
+
+    $scope.turnOnRelay=function(ch){
+        $http.get('http://192.168.2.7:3009/modbus/on/'+ch)
+        .then((response)=>{
+            console.log(response);
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+
+    $scope.turnOffRelay=function(ch){
+        $http.get('http://192.168.2.7:3009/modbus/off/'+ch)
+        .then((response)=>{
+            console.log(response);
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+
 }])
