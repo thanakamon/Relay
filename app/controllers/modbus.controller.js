4c4489fdc3a1bb35a71d4fdc1e24e04f038852f8
thanaponyalan <42196077+thanaponyalan@users.noreply.github.com>
2020-11-24 01:29:33 +0700

    Update modbus.controller.js

--- app/controllers/modbus.controller.js
+++ app/controllers/modbus.controller.js
@@ -1,6 +1,6 @@
 var modbusRTU=require('../../config/modbus');
 
-var toggleRelay=(req,res)=>{
+/*var toggleRelay=(req,res)=>{
     modbusRTU.readCoils(req.params['ch'],1).then((data)=>{
         modbusRTU.writeCoil(req.params['ch'],!data.data);
         res.send({data:data.data});
@@ -8,8 +8,26 @@ var toggleRelay=(req,res)=>{
         console.log(err);
         res.send("ERROR")
     });
+}*/
+
+var turnOnRelay=(req,res)=>{
+    modbusRTU.writeCoil(req.params['ch'],1).then((data)=>{
+        res.send(data);
+    }).catch((err)=>{
+        res.send("ERROR");
+    });
+}
+
+var turnOffRelay=(req,res)=>{
+    modbusRTU.writeCoil(req.params['ch'],0).then((data)=>{
+        res.send(data);
+    }).catch((err)=>{
+        res.send("ERROR");
+    });
 }
 
 module.exports={
-    toggleRelay
+    //toggleRelay,
+    turnOnRelay,
+    turnOffRelay
 }
